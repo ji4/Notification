b6b93e5a23afa93958fa3913ee8974b02f0814a7
ji4 <ji4@users.noreply.github.com>
2017-08-16 15:21:19 +0800

    Set The Same Button Click Listener

--- app/src/main/java/com/example/chiayingwu/notification/MainActivity.java
+++ app/src/main/java/com/example/chiayingwu/notification/MainActivity.java
@@ -12,15 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setButtonListener();
         NotifyUtil.init(getApplicationContext());
+    }
 
-        m_btn_simple = (Button) findViewById(R.id.btn_simple);
-        m_btn_simple.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NotifyUtil.buildSimple(1, R.drawable.ic_launcher, "I'm titile", "I'm content", null)
-                        .show();
+    Button.OnClickListener btnToNotify = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_simple:
+                    NotifyUtil.buildSimple(1, R.drawable.ic_launcher, "I'm titile", "I'm content", null)
+                            .show();
+                    break;
             }
-        });
+        }
+    };
+
+    private void findViews() {
+        m_btn_simple = (Button) findViewById(R.id.btn_simple);
+    }
+
+    private void setButtonListener() {
+        m_btn_simple.setOnClickListener(btnToNotify);
     }
 }
