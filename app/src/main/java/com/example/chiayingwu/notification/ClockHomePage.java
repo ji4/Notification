87ad9d5afe0df604060ac143ca13d61ee4f95dd8
ji4 <ji4@users.noreply.github.com>
2017-08-17 15:14:28 +0800

    Add ClockHomePage Activity

---
+++ app/src/main/java/com/example/chiayingwu/notification/ClockHomePage.java
@@ -0,0 +1,15 @@
+package com.example.chiayingwu.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClockHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_home_page);
+
+
+    }
+}
